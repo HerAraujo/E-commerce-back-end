fc7f7277c43eaa7c04d8abe28461c694994184f1
HerAraujo <heraraujo12@gmail.com>
2022-03-28 13:51:08 -0300

    admin CRUD

---
+++ controllers/admin/adminController.js
@@ -0,0 +1,57 @@
+const res = require("express/lib/response");
+const { Admin } = require("../../models");
+
+async function show(req, res) {
+  try {
+    const admins = await Admin.findAll();
+    res.json(admins);
+  } catch (err) {
+    res.status(404).json({ message: `Admin list not found` });
+  }
+}
+
+async function store(req, res) {
+  try {
+    const admin = await Admin.create({
+      firstname: String(req.body.firstname),
+      lastname: String(req.body.lastname),
+      password: String(req.body.password),
+      email: String(req.body.email),
+    });
+    res.json({
+      firstname: admin.firstname,
+      lastname: admin.lastname,
+    });
+  } catch (err) {
+    res.status(400).json({ message: `An error has ocurred` });
+  }
+}
+
+async function destroy(req, res) {
+  const admin = await Admin.findOne({ where: { id: String(req.params.id) } });
+  await Admin.destroy({
+    where: {
+      id: String(req.params.id),
+    },
+  });
+  admin
+    ? res.status(200).json(`${admin.firstname + " " + admin.lastname} deleted successfully`)
+    : res.json(`Admin with id: ${req.params.id} does not exist`);
+}
+
+async function update(req, res) {
+  const admin = await Admin.findByPk(req.params.id);
+  await Admin.update(
+    {
+      firstname: String(req.body.firstname),
+      lastname: String(req.body.lastname),
+      email: String(req.body.email),
+    },
+    { where: { id: req.params.id } },
+  );
+  admin
+    ? res.json(`Admin ${admin.firstname + " " + admin.lastname} updated successfully`)
+    : res.json(`Admin with id: ${req.params.id} does not exist`);
+}
+
+module.exports = { show, store, destroy, update };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 //admin
 const adminProductRoutes = require("./admin/productRoutes");
 const adminStateRoutes = require("./admin/stateRoutes");
+const adminAdminRoutes = require("./admin/adminRoutes");
 
 //shop
 const productRoutes = require("./shop/productRoutes");
@@ -10,6 +11,7 @@ const userRoutes = require("./shop/userRoutes");
 module.exports = (app) => {
   app.use("/admin", adminStateRoutes);
   app.use("/admin", adminProductRoutes);
+  app.use("/admin", adminAdminRoutes);
 
   app.use(productRoutes);
   app.use(categoryRoutes);

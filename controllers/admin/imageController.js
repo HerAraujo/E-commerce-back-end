2bd538e211daee8df49a23a71a8cb30e82d70efd
HerAraujo <heraraujo12@gmail.com>
2022-04-04 17:29:08 -0300

    images to supabase (team)

--- controllers/admin/imageController.js
+++ controllers/admin/imageController.js
@@ -15,32 +15,22 @@ async function show(req, res) {
 
 async function store(req, res) {
   try {
-    const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
     const form = formidable({
       multiples: true,
-      uploadDir: process.env.ABSOLUTE_IMAGES_FOLDER_PATH,
       keepExtensions: true,
     });
 
     form.parse(req, async (err, fields, files) => {
-      console.log(files);
-      // const ext = path.extname(files.avatar.path);
-      // const newFileName = files.image.newFilename;
-      // const { data, error } = await supabase.storage
-      //   .from("avatars")
-      //   .upload(newFileName, fs.createReadStream(files.avatar.path), {
-      //     cacheControl: "3600",
-      //     upsert: false,
-      //     contentType: files.avatar.type,
-      //   });
-
-      // form.parse(req, async (err, fields, files) => {
-      //   const image = Image.create({
-      //     name: files.image.newFilename,
-      //     title: fields.title,
-      //   });
-      //   res.json(files.image);
+      const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+      const { data, error } = await supabase.storage
+        .from("e-commerce")
+        .upload(files.image.newFilename, fs.createReadStream(files.image.filepath), {
+          cacheControl: "3600",
+          upsert: false,
+          contentType: files.image.mimetype,
+        });
+      const image = await Image.create({ name: files.image.newFilename, title: fields.title });
+      res.json(image);
     });
   } catch (err) {
     res.status(400).json({ message: "An error has ocurred" });

c6ca9a12ad7937f618776a1765c7cbbdb847f141
stephanieesquire <stephanieesquiremarco@gmail.com>
2022-04-06 09:40:39 -0300

    dialect

--- models/index.js
+++ models/index.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_CONNECTION,
-    // dialectModule: require("pg"),
+    //dialect: process.env.DB_CONNECTION,
+    dialectModule: require("pg"), //enable when cloud
     logging: false,
   },
 );

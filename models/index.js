b230aa27501f856c49545a4c74696930a2d8d1af
HerAraujo <heraraujo12@gmail.com>
2022-04-06 10:46:38 -0300

    dialect

--- models/index.js
+++ models/index.js
@@ -4,11 +4,13 @@ const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
+
   {
     host: process.env.DB_HOST,
-    //dialect: process.env.DB_CONNECTION,
+    dialect: process.env.DB_CONNECTION,
     dialectModule: require("pg"), //enable when cloud
     logging: false,
+    port: process.env.DB_PORT,
   },
 );
 

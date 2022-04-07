7d2d888d78b725e254e42d89c222455d11437ca5
stephanieesquire <stephanieesquiremarco@gmail.com>
2022-04-07 12:07:05 -0300

    test error when duplicate

--- controllers/shop/userController.js
+++ controllers/shop/userController.js
@@ -15,9 +15,10 @@ async function store(req, res) {
       lastname: user.lastname,
     });
   } catch (err) {
-    err.parent.errno === Number(process.env.ERROR_CODE_DUPLICATE_KEY)
-      ? res.status(409).json({ message: "User already exists" })
-      : res.status(400).json({ message: "An error has ocurred" });
+    res.json({ err: err.parent.errno });
+    // err.parent.errno === Number(process.env.ERROR_CODE_DUPLICATE_KEY)
+    //   ? res.status(409).json({ message: "User already exists" })
+    //   : res.status(400).json({ message: "An error has ocurred" });
   }
 }
 
